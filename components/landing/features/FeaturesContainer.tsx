90489514e93f80ae0ced05fc7a84819e732582dd
Meedux <herrerojayemark@gmail.com>
2024-07-29 02:06:04 +0800

    Features Container, i guess

---
+++ components/landing/features/FeaturesContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeaturesContainer = () => {
+  return (
+    <>
+        <div className="container mt-[10rem]">
+            
+        </div>
+    </>
+  )
+}
+
+export default FeaturesContainer
